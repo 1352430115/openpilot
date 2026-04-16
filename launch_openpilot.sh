961b62d20222435e08d054ec4c7cd3d984fb6bcd
1okko <15377594951@189.cn>
2026-04-16 20:38:24 +0800

    333

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
 
-MODEL="$(tr -d '\0' < "/sys/firmware/devicetree/base/model")"
-export MODEL
-
-if [ "$MODEL" = "comma tici" ]; then
-  # Force a failure if the launcher doesn't exist
-  [ -x "$C3_LAUNCH_SH" ] || false
-
-  # If it exists, run it
-  exec "$C3_LAUNCH_SH"
-fi
 
 exec ./launch_chffrplus.sh
