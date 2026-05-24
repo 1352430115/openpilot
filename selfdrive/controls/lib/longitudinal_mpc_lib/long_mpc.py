4371f9b482452bc61f7cd6276a2a968d4cc2c381
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2026-05-24 20:13:31 +0800

    測試偵測前方慢車提早減速

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -33,15 +33,15 @@ COST_E_DIM = 5
 COST_DIM = COST_E_DIM + 1
 CONSTR_DIM = 4
 
-X_EGO_OBSTACLE_COST = 3.
+X_EGO_OBSTACLE_COST = 3.5
 X_EGO_COST = 0.
 V_EGO_COST = 0.
-A_EGO_COST = 0.
-J_EGO_COST = 5.
-A_CHANGE_COST = 200.
+A_EGO_COST = 0.15
+J_EGO_COST = 6.
+A_CHANGE_COST = 220.
 DANGER_ZONE_COST = 100.
 CRASH_DISTANCE = .25
-LEAD_DANGER_FACTOR = 0.5
+LEAD_DANGER_FACTOR = 0.75
 LIMIT_COST = 1e6
 ACADOS_SOLVER_TYPE = 'SQP_RTI'
 
@@ -338,6 +338,9 @@ class LongitudinalMpc:
     self.source = MPC_SOURCES[np.argmin(x_obstacles[0])]
 
     self.yref[:,:] = 0.0
+    
+    self.yref[:, 3] = -1e-3
+    
     for i in range(N):
       self.solver.set(i, "yref", self.yref[i])
     self.solver.set(N, "yref", self.yref[N][:COST_E_DIM])
