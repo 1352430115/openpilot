31b5e0844200c78754fddbb6017265f962ad8dcc
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2026-05-27 10:30:34 +0800

    優化跟車距離/提前反應遠方慢速車

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -36,8 +36,8 @@ CONSTR_DIM = 4
 X_EGO_OBSTACLE_COST = 3.
 X_EGO_COST = 0.
 V_EGO_COST = 0.
-A_EGO_COST = 0.
-J_EGO_COST = 5.
+A_EGO_COST = 0.15
+J_EGO_COST = 6.
 A_CHANGE_COST = 200.
 DANGER_ZONE_COST = 100.
 CRASH_DISTANCE = .25
@@ -57,7 +57,7 @@ T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 COMFORT_BRAKE = 2.5
 STOP_DISTANCE = 6.0
 CRUISE_MIN_ACCEL = -1.2
-CRUISE_MAX_ACCEL = 1.6
+CRUISE_MAX_ACCEL = 1.2
 MIN_X_LEAD_FACTOR = 0.5
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
@@ -66,18 +66,18 @@ def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   elif personality==log.LongitudinalPersonality.standard:
     return 1.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 0.5
+    return 0.8
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
 
 def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 1.75
+    return 1.55
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.45
-  elif personality==log.LongitudinalPersonality.aggressive:
     return 1.25
+  elif personality==log.LongitudinalPersonality.aggressive:
+    return 0.95
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
@@ -338,6 +338,22 @@ class LongitudinalMpc:
     self.source = MPC_SOURCES[np.argmin(x_obstacles[0])]
 
     self.yref[:,:] = 0.0
+
+    # Dynamic closing-speed-based coasting bias
+    lead = radarstate.leadOne
+
+    bias = 0.0
+
+    if lead.status:
+      # relative speed (m/s)
+      v_rel = v_ego - lead.vLead
+
+      # only activate when approaching slower lead
+      if v_ego > 8.0 and v_rel > 0.0:
+        bias = np.interp(v_rel,[0.0, 4.0, 8.0],[0.0, -0.01, -0.03])
+                                      
+    self.yref[:, 3] = bias
+ 
     for i in range(N):
       self.solver.set(i, "yref", self.yref[i])
     self.solver.set(N, "yref", self.yref[N][:COST_E_DIM])
