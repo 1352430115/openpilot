f7c13ff58fa0a971c20156fb42639b15e71211ee
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2026-05-27 10:15:43 +0800

    個性顛倒修正

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -62,11 +62,11 @@ MIN_X_LEAD_FACTOR = 0.5
 
 def get_jerk_factor(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
-    return 0.8
+    return 1.0
   elif personality==log.LongitudinalPersonality.standard:
     return 1.0
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.0
+    return 0.8
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
