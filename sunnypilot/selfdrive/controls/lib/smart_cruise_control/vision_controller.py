85c1bce39ba5ffbb0adcdb7b55ed06d760ec8621
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2026-05-28 22:58:50 +0800

    sccv

--- sunnypilot/selfdrive/controls/lib/smart_cruise_control/vision_controller.py
+++ sunnypilot/selfdrive/controls/lib/smart_cruise_control/vision_controller.py
@@ -38,7 +38,8 @@ _ENTERING_SMOOTH_DECEL_BP = [1.1, 3.]  # absolute value of lat acc ahead
 
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
-_TURNING_ACC_V = [0.35, 0.12, -0.05]  # acc value
+#_TURNING_ACC_V = [0.25, 0.05, -0.1]  # acc value 大彎速度不夠
+_TURNING_ACC_V = [0.25, 0.08, -0.08]  # acc value
 _TURNING_ACC_BP = [1.5, 2.3, 3.]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.7  # Conformable acceleration to regain speed while leaving a turn.
