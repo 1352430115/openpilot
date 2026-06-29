625685c3caab97f69f5afd8c12f15e9ac01ca490
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2026-06-29 12:09:00 +0800

    SCCV視覺彎道減速 V1

--- sunnypilot/selfdrive/controls/lib/smart_cruise_control/vision_controller.py
+++ sunnypilot/selfdrive/controls/lib/smart_cruise_control/vision_controller.py
@@ -33,14 +33,14 @@ _NO_OVERSHOOT_TIME_HORIZON = 4.  # s. Time to use for velocity desired based on 
 
 # Lookup table for the minimum smooth deceleration during the ENTERING state
 # depending on the actual maximum absolute lateral acceleration predicted on the turn ahead.
-_ENTERING_SMOOTH_DECEL_V = [-0.15, -0.7]  # min decel value allowed on ENTERING state
+_ENTERING_SMOOTH_DECEL_V = [-0.10, -0.5]  # min decel value allowed on ENTERING state
 _ENTERING_SMOOTH_DECEL_BP = [1.1, 3.]  # absolute value of lat acc ahead
 
 # Lookup table for the acceleration for the TURNING state
 # depending on the current lateral acceleration of the vehicle.
 #_TURNING_ACC_V = [0.25, 0.05,/0.08, -0.1/-0.08]  # acc value 持續大彎中加速度不夠,特大彎減速過多
-_TURNING_ACC_V = [0.26, 0.10, -0.1]  # acc value
-_TURNING_ACC_BP = [1.5, 2.3, 4.]  # absolute value of current lat acc
+_TURNING_ACC_V = [0.25, 0.10, -0.05]  # acc value
+_TURNING_ACC_BP = [1.5, 2.3, 3.]  # absolute value of current lat acc
 
 _LEAVING_ACC = 0.7  # Conformable acceleration to regain speed while leaving a turn.
 
