35875ade505301b58ec023ccca7bbc6eacfa674f
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2026-06-30 11:46:20 +0800

    Fix variable reference for stockAeb calculation

--- opendbc_repo/opendbc/car/toyota/carstate.py
+++ opendbc_repo/opendbc/car/toyota/carstate.py
@@ -93,7 +93,7 @@ class CarState(CarStateBase, CarStateExt):
       ret.gasPressed = cp.vl["PCM_CRUISE"]["GAS_RELEASED"] == 0
       can_gear = int(cp.vl["GEAR_PACKET"]["GEAR"])
       if not self.CP.flags & ToyotaFlags.DISABLE_RADAR.value:
-        ret.stockAeb = bool(cp_acc.vl["ACC_CONTROL"]["PRECOLLISION_ACTIVE"] and cp_acc.vl["PRE_COLLISION"]["FORCE"] < -1e-5)
+        ret.stockAeb = bool(cp_acc.vl["PRE_COLLISION"]["PRECOLLISION_ACTIVE"] and cp_acc.vl["PRE_COLLISION"]["FORCE"] < -1e-5)
 
     self.parse_wheel_speeds(ret,
       cp.vl["WHEEL_SPEEDS"]["WHEEL_SPEED_FL"],
