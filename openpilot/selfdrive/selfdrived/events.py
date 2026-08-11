1e2434d2d6ad9ad6676562956a6d52b5f196e007
yaya <39397315+1352430115@users.noreply.github.com>
2026-08-11 16:28:13 +0800

    Update warning message for lane change blocked event

--- openpilot/selfdrive/selfdrived/events.py
+++ openpilot/selfdrive/selfdrived/events.py
@@ -440,7 +440,7 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
 
   EventName.laneChangeBlocked: {
     ET.WARNING: Alert(
-      "盲点检测到车辆",
+      "道路边沿阻止变道",
       "",
       AlertStatus.userPrompt, AlertSize.small,
       Priority.LOW, VisualAlert.none, AudibleAlert.prompt, .1),
