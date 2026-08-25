9b01e756f12b43a9827b9956951fbf1efdc36b5c
yaya <39397315+1352430115@users.noreply.github.com>
2026-08-25 10:45:46 +0800

    Decrease minimum lane probability threshold to 0.3

--- openpilot/selfdrive/controls/lib/lane_centering.py
+++ openpilot/selfdrive/controls/lib/lane_centering.py
@@ -6,7 +6,7 @@ from openpilot.selfdrive.controls.lib.drive_helpers import smooth_value
 
 
 _MIN_V_EGO = 5.0
-_MIN_LANE_PROB = 0.4
+_MIN_LANE_PROB = 0.3
 _MAX_LANE_STD = 0.6
 _MIN_LANE_WIDTH = 2.6
 _MAX_LANE_WIDTH = 4.8
